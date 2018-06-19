51342770652b513a1051823ef8428a5b817bd03a
Deepak Choubey <deepakchoubey.india@gmail.com>
2018-06-19 09:09:12 +0530

    Initial Commit

---
+++ src/main/java/com/deepak/appCreater/SpringBootAngularAppCreaterApplication.java
@@ -0,0 +1,12 @@
+package com.deepak.appCreater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularAppCreaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularAppCreaterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/deepak/appCreater/SpringBootAngularAppCreaterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.deepak.appCreater;
+
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class SpringBootAngularAppCreaterApplicationTests {
+
+//	@Test
+	public void contextLoads() {
+	}
+
+}
